27cbb380a8429f7a8709525daf6fba92b6e70686
SAIF Ali <41753389+saifali7@users.noreply.github.com>
2025-08-19 19:42:42 +0000

    variables

---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,19 @@
+const accountid = 134256
+let accountemail ="saifali@google.com"
+var accountpassword = "12345"
+accountcity = "delhi"
+let accountstate;
+
+// accountid = 2  //  not allowed
+
+//accountemail = "hyt@gh.com"
+//accountpassword = "654321"
+//accountcity = "punjab"
+
+console.log(accountid);
+
+console.table([accountid, accountemail, accountpassword, accountcity, accountstate,]);
+
+/*
+    never use to var becouse of issue in block and functional scope
+*/
