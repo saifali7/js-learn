19cf396a35ecc513c576f41398184798eb85232b
SAIF Ali <41753389+saifali7@users.noreply.github.com>
2025-08-20 14:31:53 +0000

    data type

---
+++ .devcontainer/basic/datatype.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3+3)  // we are using nodejs, not browser
+
+
+let name = "saif ali"
+let age = 18
+let isloggedin = false
+
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+
+//console.log(typeof "saif");  //string
+
+//console.log(typeof age);  //number   let age = 18
+
+//console.log(typeof null);  //object
+
+//console.log(typeof undefined); //undefined
+
+
+
+
+
+
